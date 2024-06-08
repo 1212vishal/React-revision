8a57abfc009e4ba2708bc34603673d2f1ef42579
vishal kumar <vkumar971660@gmail.com>
2024-06-08 18:12:21 +0530

    episode-6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,26 +1,59 @@
 import ResturantCard from "./ResturantCrad";
-import resInfo from "../utils/mockData";
-import { useState } from "react";
+import Shimmer from "./Shimmer";
+
+import { useState, useEffect} from "react";
 
 
 const Body=()=>{ 
-   const [resList,setresList]=useState(resInfo);
-    return(
+  // console.log("body Render")
+   const [resList,setresList]=useState([]);
+   const [dupResList,setdupResList]=useState([]);
+   const [searchText,setsearchText]=useState("");
+
+     
+    useEffect(()=>{
+        fetchData();
+   },[]);
+  
+  const fetchData=async()=>{
+      const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.572646&lng=88.36389500000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+      const json=await data.json();
+      //console.log(json);
+      //console.log(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle);
+      setresList(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+      setdupResList(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+  }
+
+  //  if(resList.length===0)
+  //   {
+  //     return <Shimmer/>
+  //   }
+
+    return resList.length===0?<Shimmer/>:(
         <div className="body">
 
             <div className="search">
-             <input className="Serach-box" type="Serach Resturant" />
+
+             <input className="Serach-box" type="Serach Resturant"  value={searchText} onChange={(e)=>{
+                setsearchText(e.target.value);
+             }}/>
+
+             <button className="Fiter-Resturant" onClick={()=>{
+                const filterResList=resList.filter((res)=>res.info.name.toLowerCase().includes(searchText.toLowerCase()));
+                setdupResList(filterResList);
+             }}>Search</button>
+
              <button className="Search-Button" 
              onClick={()=>{
                 const filterResturant=resList.filter((res)=>(res.info.avgRating>=4));
-                setresList(filterResturant);
+                setdupResList(filterResturant);
                }}
              >Top Rated Resturant</button>
             </div>
 
             <div className="card-items">
               {
-                resList.map((resturant)=>(<ResturantCard key={resturant.info.id} resData={resturant}/>))
+                dupResList.map((resturant)=>(<ResturantCard key={resturant.info.id} resData={resturant}/>))
               }
             </div>
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 import { LOGO_URL } from "../utils/Constant";
-
+import { useState } from "react";
 const Header=()=>{
+
+    const [btnName,setbtnName]=useState("Login");
+
     return(
         <div className="header">
         <div>
@@ -12,7 +15,10 @@ const Header=()=>{
                 <ul>Home</ul>
                 <ul>About Us</ul>
                 <ul>Conatact Us</ul>
-                <ul>Login</ul>
+                <ul>Cart</ul>
+                <button className="login" onClick={()=>{
+                    btnName==="Login"?setbtnName("Logout"):setbtnName("Login");
+                }}>{btnName}</button>
             </li>
         </div>
         </div>
